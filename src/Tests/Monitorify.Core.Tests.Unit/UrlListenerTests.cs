1c50368f36787de0a8708eac966c28c80df273f2
Eduard <eduard.truuvaart@just-eat.com>
2016-07-14 16:24:41 +0100

    unit tests

---
+++ src/Tests/Monitorify.Core.Tests.Unit/UrlListenerTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Monitorify.Core.Tests.Unit
+{
+    [TestFixture]
+    public class UrlListenerTests
+    {
+        [Test]
+        public void PassingTest()
+        {
+            Assert.IsTrue(4.Equals(4));
+        }
+    }
+}

f6761076b98af33d604cd9e74ac9bd2e49496ff1
Eduard <eduard.truuvaart@hotmail.com>
2016-07-15 20:48:59 +0100

    mocking

--- src/Tests/Monitorify.Core.Tests.Unit/UrlListenerTests.cs
+++ src/Tests/Monitorify.Core.Tests.Unit/UrlListenerTests.cs
@@ -1,13 +1,36 @@
-﻿using Xunit;
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Monitorify.Core.HttpWrapper;
+using Moq;
+using Xunit;
 
 namespace Monitorify.Core.Tests.Unit
 {
     public class UrlListenerTests
     {
         [Fact]
-        public void PassingTest()
+        public void StartListening_EndpointIsValid_HttpRequestIsMade()
         {
-            Assert.True(4.Equals(4));
+            // Arrange
+            Mock<IHttpClient> httpMock = new Mock<IHttpClient>();
+
+            HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+            httpMock.Setup(x => x.GetAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(responseMessage));
+
+            EndPoint endPoint = new EndPoint();
+            endPoint.Url = "http://www.google.com";
+            endPoint.Name = "Google";
+
+            IUrlListener listener = new UrlListener(endPoint);
+
+            // Act
+            listener.StartListening(httpMock.Object, TimeSpan.FromSeconds(3));
+
+            // Assert
+            httpMock.Verify(x => x.GetAsync(endPoint.Url), Times.AtLeastOnce);
         }
     }
 }

2510571ec9705e43af4a732c2916b420be7505f8
Eduard <eduard.truuvaart@hotmail.com>
2016-07-15 21:21:55 +0100

    more tests

--- src/Tests/Monitorify.Core.Tests.Unit/UrlListenerTests.cs
+++ src/Tests/Monitorify.Core.Tests.Unit/UrlListenerTests.cs
@@ -32,5 +32,55 @@ namespace Monitorify.Core.Tests.Unit
             // Assert
             httpMock.Verify(x => x.GetAsync(endPoint.Url), Times.AtLeastOnce);
         }
+
+        [Fact]
+        public void StartListening_EndpointIsOnline_OnlineEventIsRised()
+        {
+            // Arrange
+            Mock<IHttpClient> httpMock = new Mock<IHttpClient>();
+
+            HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.OK);
+            httpMock.Setup(x => x.GetAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(responseMessage));
+
+            EndPoint endPoint = new EndPoint();
+            endPoint.Url = "http://www.google.com";
+            endPoint.Name = "Google";
+
+            bool endpointIsOnline = false;
+            IUrlListener listener = new UrlListener(endPoint);
+            listener.ReportedOnline += point => endpointIsOnline = true;
+
+            // Act
+            listener.StartListening(httpMock.Object, TimeSpan.FromSeconds(3));
+
+            // Assert
+            Assert.True(endpointIsOnline);
+        }
+
+        [Fact]
+        public void StartListening_EndpointIsOffline_OfflineEventIsRised()
+        {
+            // Arrange
+            Mock<IHttpClient> httpMock = new Mock<IHttpClient>();
+
+            HttpResponseMessage responseMessage = new HttpResponseMessage(HttpStatusCode.NotFound);
+            httpMock.Setup(x => x.GetAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(responseMessage));
+
+            EndPoint endPoint = new EndPoint();
+            endPoint.Url = "http://www.dodgyurl.com";
+            endPoint.Name = "Google";
+
+            bool endpointIsOffline = false;
+            IUrlListener listener = new UrlListener(endPoint);
+            listener.ReportedOffline += point => endpointIsOffline = true;
+
+            // Act
+            listener.StartListening(httpMock.Object, TimeSpan.FromSeconds(3));
+
+            // Assert
+            Assert.True(endpointIsOffline);
+        }
     }
 }

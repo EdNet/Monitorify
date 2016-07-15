4dd985277d5823446d1c3989476ee29fdfd55b78
Eduard <eduard.truuvaart@hotmail.com>
2016-07-15 21:08:26 +0100

    moq

--- src/Monitorify.Core/Properties/AssemblyInfo.cs
+++ src/Monitorify.Core/Properties/AssemblyInfo.cs
@@ -10,6 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Monitorify.Core")]
 [assembly: AssemblyTrademark("")]
 [assembly: InternalsVisibleTo("Monitorify.Core.Tests.Unit")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from

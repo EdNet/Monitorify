ba44e6200f3e940d74bcc68db6d199c9a9780303
Eduard <eduard.truuvaart@just-eat.com>
2016-07-26 17:11:18 +0100

    publisher work in progress

--- src/Monitorify.Publisher/IMonitorifyNotifier.cs
+++ src/Monitorify.Publisher/IMonitorifyNotifier.cs
@@ -1,7 +1,9 @@
-﻿namespace Monitorify.Publisher
+﻿using System.Threading.Tasks;
+
+namespace Monitorify.Publisher
 {
     public interface IMonitorifyNotifier
     {
-        
+        Task TrackAndNotify();
     }
 }

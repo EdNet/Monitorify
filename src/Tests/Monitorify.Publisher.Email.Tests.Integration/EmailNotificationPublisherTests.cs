da2285210a0b6146162a40a064cb30a398fc7e30
Eduard <eduard.truuvaart@just-eat.com>
2016-08-08 17:33:56 +0100

    integration tests

---
+++ src/Tests/Monitorify.Publisher.Email.Tests.Integration/EmailNotificationPublisherTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Monitorify.Core;
+using Xunit;
+
+namespace Monitorify.Publisher.Email.Tests.Integration
+{
+    public class EmailNotificationPublisherTests
+    {
+        [Fact]
+        public async void SendMessage_SmtpIsConfiguredCorrectly_MessageIsSent()
+        {
+            // Arrange
+            var endpoint = new EndPoint { Name = "Google", Url = "https://www.google.com", LastOutageTimeSpan = DateTime.UtcNow.AddMinutes(-1) - DateTime.UtcNow.AddMinutes(-2) };
+            var config = new EmailNotificationPublisherConfig("smtp-mail.outlook.com", 587, true, "monitorify@outlook.com", "********", "monitorify@outlook.com", "monitorify@outlook.com");
+            var publisher = new EmailNotificationPublisher(config);
+
+            // Act / Assert
+            await publisher.NotifyBackOnline(endpoint);
+        }
+    }
+}

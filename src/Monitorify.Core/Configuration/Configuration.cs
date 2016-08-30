cb153d1fb9a290c9fa3f7cb1dee12a6fdf249c8d
Eduard <eduard.truuvaart@just-eat.com>
2016-08-30 14:43:48 +0100

    configuration file is added

---
+++ src/Monitorify.Core/Configuration/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Monitorify.Core.Configuration
+{
+    public class Configuration : IConfiguration
+    {
+        public IEnumerable<EndPoint> EndPoints { get; set; }
+        public TimeSpan PingDelay { get; set; }
+    }
+}
